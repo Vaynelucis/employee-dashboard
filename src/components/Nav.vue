088a637aa2d652108a48a6edca1c044bcea9745e
Benard Obinna <bernardobinna2@gmail.com>
2019-09-09 17:40:38 +0100

    working on nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- <nav>
+  </nav>-->
+  <div class="row">
+    <div class="col m2">
+      <h5>BOARDASH</h5>
+    </div>
+    <div class="col m6">
+      <input class="browser-default" type="text" placeholder="Search message, people, ticket">
+    </div>
+    <div class="col m2">
+      <p>Set Alert</p>
+    </div>
+    <div class="col m2">
+      <p>username</p>
+    </div>
+  </div>
+</template>
