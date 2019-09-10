6fcc5d23fd22ff696d3876393ce097574bb686b3
Benard Obinna <bernardobinna2@gmail.com>
2019-09-11 00:47:40 +0100

    sstyled and aligned the message component

--- src/components/main_body_components/MainBodyMessage.vue
+++ src/components/main_body_components/MainBodyMessage.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>
-    <p>blahh</p>
+  <div class="row main-body-message">
+    <div class="col m4 message">
+      <img class="circle" src="../../assets/logo.png" alt>
+      <div>
+        <span>Overall Messages</span>
+        <h6>
+          2389
+          <span>This Month</span>
+        </h6>
+      </div>
+      <span class="line"></span>
+    </div>
+    <div class="col m4 message">
+      <img class="circle" src="../../assets/logo.png" alt>
+      <div>
+        <span>Overall Messages</span>
+        <h6>
+          2389
+          <span>This Month</span>
+        </h6>
+      </div>
+    </div>
+    <div class="col m4 message">
+      <span class="line"></span>
+      <img class="circle" src="../../assets/logo.png" alt>
+      <div>
+        <span>Overall Messages</span>
+        <h6>
+          2389
+          <span>This Month</span>
+        </h6>
+      </div>
+    </div>
   </div>
 </template>
