3b89d533879474b3233906291694d2bf15fb1902
Benard Obinna <bernardobinna2@gmail.com>
2019-09-11 00:14:48 +0100

    added the main body message component

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -3,16 +3,19 @@
     <!-- <p>blahh</p> -->
     <MainBodyNav/>
     <MainBodyCall/>
+    <MainBodyMessage/>
   </div>
 </template>
 
 <script>
 import MainBodyNav from "./main_body_components/MainBodyNav.vue";
 import MainBodyCall from "./main_body_components/MainBodyCall.vue";
+import MainBodyMessage from "./main_body_components/MainBodyMessage.vue";
 export default {
   components: {
     MainBodyNav,
-    MainBodyCall
+    MainBodyCall,
+    MainBodyMessage
   }
 };
 </script>
