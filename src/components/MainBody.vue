712807cd5d16d15c391fec03955c22fdddf54fac
Benard Obinna <bernardobinna2@gmail.com>
2019-09-10 20:57:13 +0100

    split main body components

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <p>blahh</p>
+  <div class="main-body">
+    <!-- <p>blahh</p> -->
+    <MainBodyNav/>
   </div>
 </template>
+
+<script>
+import MainBodyNav from "./main_body_components/MainBodyNav.vue";
+export default {
+  components: {
+    MainBodyNav
+  }
+};
+</script>
---
+++ src/components/main_body_components/MainBodyNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="row main-body-nav">
+    <div class="col m6">
+      <h5>Messages</h5>
+    </div>
+    <div class="col m3">
+      <div class="my-pagination">
+        <a href="#!">
+          <i class="material-icons">chevron_left</i>
+        </a>
+        <p>1 - 50 Pages</p>
+        <a href="#!">
+          <i class="material-icons">chevron_right</i>
+        </a>
+      </div>
+      <!-- <ul class="pagination">
+        <li class="waves-effect">
+          <a href="#!">
+            <i class="material-icons">chevron_left</i>
+          </a>
+        </li>
+        <li class="waves-effect">
+          <a href="#!">1 - 50 Pages</a>
+        </li>
+
+        <li class="waves-effect">
+          <a href="#!">
+            <i class="material-icons">chevron_right</i>
+          </a>
+        </li>
+      </ul>-->
+    </div>
+    <div class="col m1">
+      <button class="btn">All</button>
+    </div>
+    <div class="col m2 button-group">
+      <button class="button">Unread</button>
+      <button class>Important</button>
+    </div>
+  </div>
+</template>
