cedf87655f18195b73bcff2befa74060c79f6df6
Benard Obinna <bernardobinna2@gmail.com>
2019-09-11 06:22:10 +0100

    debugingg radio button

--- src/components/main_body_components/MainBodyList.vue
+++ src/components/main_body_components/MainBodyList.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="row main-body-list">
-    <div class="col m1">
-      <input type="checkbox">
-    </div>
-    <div class="col m1 avater">
-      <img src="../../assets/logo.png" alt>
-    </div>
-    <div class="col m2 full-name">
-      <p>
-        <span>First Name</span>
-        <span>Last Name</span>
-      </p>
-    </div>
-    <div class="col m2 status">
-      <p>User Testing</p>
-    </div>
-    <div class="col m5 message-intro">
-      <p>Hi james, we need to do user testing by monday, please add me in slack group ...</p>
-    </div>
-    <div class="col m1 menu-icon">
-      <p>icon</p>
+    <div id="individual-message" class="row">
+      <div class="col m1">
+        <label>
+          <input type="checkbox">
+          <span></span>
+        </label>
+      </div>
+      <div class="col m1 avater">
+        <img src="../../assets/logo.png" alt>
+      </div>
+      <div class="col m2 full-name">
+        <p>
+          <span>First Name</span>
+          <span>Last Name</span>
+        </p>
+      </div>
+      <div class="col m2 status">
+        <p>User Testing</p>
+      </div>
+      <div class="col m5 message-intro">
+        <p>Hi james, we need to do user testing by monday, please add me in slack group ...</p>
+      </div>
+      <div class="col m1 menu-icon">
+        <p>icon</p>
+      </div>
     </div>
   </div>
 </template>
