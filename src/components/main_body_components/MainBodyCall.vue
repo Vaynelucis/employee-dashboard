93cc8284b8b4895312bde1e000c85f3967884da9
Benard Obinna <bernardobinna2@gmail.com>
2019-09-11 23:20:50 +0100

    did the main body call  section with flexbox

--- src/components/main_body_components/MainBodyCall.vue
+++ src/components/main_body_components/MainBodyCall.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="row main-body-call">
-    <div class="col m5 call-banner">
+    <!-- <div class="col m5 call-banner"> -->
+    <div class="call-banner">
       <img class="circle" src="http://lorempixel.com/400/400/people" alt>
       <div>
         <h6>Sales Presentation - Live Group Call</h6>
@@ -8,7 +9,8 @@
         <p>started # mins ago</p>
       </div>
     </div>
-    <div class="col m2 participants">
+    <div class="participants">
+      <!-- <div class="col m2 participants"> -->
       <p>A</p>
       <p>K</p>
       <p>D</p>
@@ -16,10 +18,12 @@
 
       <p id="more">+5 more</p>
     </div>
-    <div class="col m3 invitation">
+    <!-- <div class="col m3 invitation"> -->
+    <div class="invitation">
       <button>You're Invited</button>
     </div>
-    <div class="col m2 join-call">
+    <!-- <div class="col m2 join-call"> -->
+    <div class="join-call">
       <i class="material-icons">videocam</i>
       <p>Join Now</p>
     </div>
