591a5d10c14ddf0044fa55742b859df560b89c20
Benard Obinna <bernardobinna2@gmail.com>
2019-09-16 13:14:46 +0100

    cleared comments from the mainbodynav.vue file

--- src/components/main_body_components/MainBodyNav.vue
+++ src/components/main_body_components/MainBodyNav.vue
@@ -1,11 +1,8 @@
 <template>
-  <!-- <div class="row main-body-nav"> -->
   <div class="main-body-nav">
-    <!-- <div class="col m5"> -->
     <div class="nav-heading">
       <h5>Messages</h5>
     </div>
-    <!-- <div class="col m3"> -->
     <div class>
       <div class="my-pagination">
         <a href="#!">
@@ -16,28 +13,10 @@
           <i class="material-icons">chevron_right</i>
         </a>
       </div>
-      <!-- <ul class="pagination">
-        <li class="waves-effect">
-          <a href="#!">
-            <i class="material-icons">chevron_left</i>
-          </a>
-        </li>
-        <li class="waves-effect">
-          <a href="#!">1 - 50 Pages</a>
-        </li>
-
-        <li class="waves-effect">
-          <a href="#!">
-            <i class="material-icons">chevron_right</i>
-          </a>
-        </li>
-      </ul>-->
     </div>
-    <!-- <div class="col m1"> -->
     <div class="all-btn">
       <button class="button">All</button>
     </div>
-    <!-- <div class="col m2 button-group"> -->
     <div class="button-group">
       <button class="button">Unread</button>
       <button class="button">Important</button>
