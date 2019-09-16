1884bbdd90dfc5b87b381a7e7bc3b630d536d47d
Benard Obinna <bernardobinna2@gmail.com>
2019-09-16 13:11:42 +0100

    cleared out comments from app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App "/> -->
+  
     <Nav :currentUser="currentUser"/>
-    <!-- <div class="row body"> -->
+   
     <div class="main-body-grid">
-      <!-- <div class="col"> -->
-      <!-- <div class="sidebar-container-grid"> -->
+    
       <Sidebar :currentUser="currentUser"/>
-      <!-- </div> -->
-      <!-- <div class="col m9"> -->
-      <!-- <div class="main-body-container-grid"> -->
+     
       <MainBody/>
       <a class="btn-floating btn-large waves-effect waves-light red fab">
         <i class="material-icons">add</i>
       </a>
-      <!-- </div> -->
+    
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Nav from "./components/Nav.vue";
 import Sidebar from "./components/Sidebar.vue";
 import MainBody from "./components/MainBody.vue";
@@ -30,7 +25,6 @@ import "./styles/style.css";
 export default {
   name: "app",
   components: {
-    HelloWorld,
     Nav,
     Sidebar,
     MainBody
@@ -53,8 +47,8 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
+ 
   color: #2c3e50;
-  // margin-top: 60px;
+ 
 }
 </style>
